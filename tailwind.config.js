5b7831c4fd1e54f45237508dd87867fcb6864c42
Cytens-Night <businesstopman@gmail.com>
2025-04-06 12:43:42 +0000

    new update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss"
+import animate from "tailwindcss-animate"
+
+const config: Config = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -73,5 +75,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 }
+
+export default config
